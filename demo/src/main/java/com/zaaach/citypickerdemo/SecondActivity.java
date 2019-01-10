852d0ae10c84262a345a735d28429da422202245
szhdev <1051687255@qq.com>
2019-01-10 19:24:50 +0800

    删除无用代码

--- demo/src/main/java/com/zaaach/citypickerdemo/SecondActivity.java
+++ demo/src/main/java/com/zaaach/citypickerdemo/SecondActivity.java
@@ -15,13 +15,6 @@ import com.zaaach.citypicker.model.LocateState;
 public class SecondActivity extends AppCompatActivity  {
     private CityPickerFragment cityPickerFragment;
 
-//    //声明AMapLocationClient类对象
-//    public AMapLocationClient mLocationClient = null;
-//
-//
-//    //声明AMapLocationClientOption对象
-//    public AMapLocationClientOption mLocationOption = null;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,47 +46,7 @@ public class SecondActivity extends AppCompatActivity  {
                 .add(R.id.fl_activity_city_picker_container, cityPickerFragment).commit();
 
 
-     /*   //定位  高德地图
-        mLocationClient.setLocationListener(new AMapLocationListener() {
-            @Override
-            public void onLocationChanged(AMapLocation aMapLocation) {
-
-                if (aMapLocation.getErrorCode() == 0) {
-                    String city = aMapLocation.getCity();
-                    String district = aMapLocation.getDistrict();
-                    String location = StringUtils.extractLocation(city, district);
-
-                    //定位成功，更新状态
-                    cityPickerFragment.updateLocateState(LocateState.SUCCESS, location.replaceAll("市", ""));
-                } else {
-                    cityPickerFragment.updateLocateState(LocateState.FAILED, null);
-
-                }
-
-
-            }
-        });*/
     }
-
-   /* //声明定位回调监听器
-    public AMapLocationListener mLocationListener = new AMapLocationListener() {
-        @Override
-        public void onLocationChanged(AMapLocation amapLocation) {
-            if (amapLocation != null) {
-                if (amapLocation.getErrorCode() == 0) {
-                    mLocationClient.stopLocation();
-                    cityPickerFragment.updateLocateState(LocateState.SUCCESS, amapLocation.getCity().replaceAll("市", ""));
-//可在其中解析amapLocation获取相应内容。
-                } else {
-                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
-                    Log.e("AmapError", "location Error, ErrCode:"
-                            + amapLocation.getErrorCode() + ", errInfo:"
-                            + amapLocation.getErrorInfo());
-                }
-            }
-        }
-    };*/
-
 
     @Override
     protected void onDestroy() {
